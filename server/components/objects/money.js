acd8b7fff8dec2a698ab051c47c4c5146773b037
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-01-18 13:03:44 -0500

    money object

--- server/components/objects/money.js
+++ server/components/objects/money.js
@@ -1,39 +1,44 @@
-class Money{
-    constructor(amount){
+class Money {
+    constructor(amount) {
         let money = this.splitCurr(amount);
         this.dollar = parseInt(money[0]);
         this.cent = parseInt(money[1]);
     }
-    splitCurr(amount){
+    splitCurr(amount) {
         let re = new RegExp('^([0-9]+)\.([0-9][0-9])$');
-        if(!re.test(amount)){
+        if (!re.test(amount)) {
             throw new Error("Invalid amount pattern! Please only enter a number with pattern '([0-9]+)\.([0-9][0-9])'");
         }
         return amount.split(".");
     }
-    increase(amount){
-        amount = this.splitCurr(amount);
-        this.cent += amount.cent;
-        while(this.cent > 99){
+    increase(amount) {
+        let change = this.splitCurr(amount).map(Number);
+        this.cent += change[1];
+        this.dollar += change[0];
+        while (this.cent > 99) {
             this.cent -= 100;
             this.dollar++;
         }
-        this.dollar += amount.dollar;
         return true;
     }
-    decrease(amount){
-        amount = this.splitCurr(amount);
-        this.cent -= amount.cent;
-        while (this.cent < 0){
+    decrease(amount) {
+        let change = this.splitCurr(amount).map(Number);
+        if ((this.dollar - change[0]) - (change[1] / 100) < 0) {
+            throw new Error("Negative prices not allowed!");
+        }
+        this.dollar -= change[0];
+        this.cent -= change[1];
+        while (this.cent < 0) {
             this.cent += 100;
             this.dollar--;
         }
-        this.dollar -= amount.dollar;
-        if (this.dollar < 0){
-            throw new Error("Negative prices not allowed!");
-        }
+
         return true;
     }
 }
+
+// let mon = new Money("15.37");
+// mon.decrease("5.38");
+// console.log([mon.dollar, mon.cent]);
 
 module.exports = Money;
